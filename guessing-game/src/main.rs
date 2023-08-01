f498f62bb9af33377cf90d49a0a8a01e9b47e532
Chris Mawdesley <chris.mawdesley@gluckgames.com>
2023-08-01 21:52:36 +0100

    fin https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let n = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Guess a number, punk...");
+
+        
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&n) {
+            Ordering::Less => println!("too smol"),
+            Ordering::Greater => println!("too chonki"),
+            Ordering::Equal => {
+                println!("jus right");
+                break;
+            }
+        }
+    }
 }
